bb47544fe39260fecb688c8102a330128fb70290
Raúl Olán <raulolan@hotmail.com>
2019-03-17 11:36:59 -0600

    Reto 7 finalizado

---
+++ reto7/Diagrama.java
@@ -0,0 +1,31 @@
+package reto7;
+
+public class Diagrama {
+    private char aglo;
+    public String arreglo;
+    public double cantidad;
+
+    public char getAglo() {
+        return aglo;
+    }
+
+    public void setAglo(char aglo) {
+        this.aglo = aglo;
+    }
+
+    public String getArreglo() {
+        return arreglo;
+    }
+
+    public void setArreglo(String arreglo) {
+        this.arreglo = arreglo;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+}
